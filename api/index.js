ba9aef7417f9007ee252640a8f76c28aad57c371
Ali Hanafiah <hanafiaha308@gmail.com>
2023-03-03 16:09:59 +0700

    Dsadsa

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const bio = {
+    nama : "ali hanafiah"
+}
+app.get("/api.get",(req,res)=>{
+    res.send(bio)
+});
+
+
+app.listen(3000);
+
+
